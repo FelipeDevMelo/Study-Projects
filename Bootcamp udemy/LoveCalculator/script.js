7b3faf63bf09ff5d8a76852103066f581a0092a3
Carla Lopes <73553939+FelipeDevMelo@users.noreply.github.com>
2022-09-05 11:59:52 -0300

    simon game

---
+++ Bootcamp udemy/LoveCalculator/script.js
@@ -0,0 +1,14 @@
+function CalculateLove(){
+    n = Math.random()
+    n*=100
+    n=Math.floor(n)+1
+    return n
+
+}
+
+
+
+
+function Increase(){
+    document.querySelector("h1").classList.toggle("huge");
+}
