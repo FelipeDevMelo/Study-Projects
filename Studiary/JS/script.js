b7b1d7269d193bf1b0b6655a5f86f6c218ccc9e2
luizfelipemelo001@gmail.com <luizfelipe2011@gmail.com>
2023-02-18 12:03:31 -0300

    test

---
+++ Studiary/JS/script.js
@@ -0,0 +1,55 @@
+function MouseOver(){
+var botaoCronograma= document.getElementById("botão1")
+
+botaoCronograma.style.backgroundColor = "  rgb(187, 226, 233)"
+
+}
+
+function MouseOut(){
+
+    var botaoCronograma= document.getElementById("botão1")
+
+botaoCronograma.style.backgroundColor = " blanchedalmond"
+}
+
+function MouseOver1(){
+    var botaoCronograma= document.getElementById("botão2")
+    
+    botaoCronograma.style.backgroundColor = "  rgb(187, 226, 233)"
+    
+    }
+    
+    function MouseOut1(){
+    
+        var botaoCronograma= document.getElementById("botão2")
+    
+    botaoCronograma.style.backgroundColor = " blanchedalmond"
+    }
+
+    function MouseOver2(){
+        var botaoCronograma= document.getElementById("botão3")
+        
+        botaoCronograma.style.backgroundColor = "  rgb(187, 226, 233)"
+        
+        }
+        
+        function MouseOut2(){
+        
+            var botaoCronograma= document.getElementById("botão3")
+        
+        botaoCronograma.style.backgroundColor = " blanchedalmond"
+        }
+
+        function MouseOver3(){
+            var botaoCronograma= document.getElementById("botão4")
+            
+            botaoCronograma.style.backgroundColor = "  rgb(187, 226, 233)"
+            
+            }
+            
+            function MouseOut3(){
+            
+                var botaoCronograma= document.getElementById("botão4")
+            
+            botaoCronograma.style.backgroundColor = " blanchedalmond"
+            }
